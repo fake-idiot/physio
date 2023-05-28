bf532181771d51d1c1696f01d0ee23bc69ed4088
Muhammad Hamza <muhammad.hamza@invozone.com>
2023-05-28 14:57:19 +0500

    implement time_slot error

--- lib/physio_web/live/user_live/appointment_live/form_component.ex
+++ lib/physio_web/live/user_live/appointment_live/form_component.ex
@@ -15,23 +15,47 @@ defmodule PhysioWeb.UserLive.AppointmentLive.FormComponent do
      |> assign(assigns)
      |> assign(changeset: changeset,
       doctor_option: doctor_option,
-      disable: disable
+      disable: disable,
+      already_taken: false
      )
     }
   end
 
   @impl true
   def handle_event("validate", %{"appointment" => appointment_params}, socket) do
-    changeset =
-      socket.assigns.appointment
-      |> Appointments.change_appointment(appointment_params)
-      |> Map.put(:action, :validate)
+    IO.inspect(appointment_params, label: "appointment_params")
+    doctor_appointments = Appointments.upcoming_appointments_by_doctor_id(String.to_integer(appointment_params["doctor_id"]))
 
-    {:noreply, assign(socket, :changeset, changeset)}
+    IO.inspect(Enum.empty?(doctor_appointments), label: "Enum.empty?(doctor_appointments)")
+    IO.inspect(appointment_params["date"], label: "appointment_paramsdate")
+    IO.inspect(appointment_params["time"], label: "appointment_paramstime")
+
+    if !Enum.empty?(doctor_appointments) and
+        appointment_params["date"] != "" and
+        check_appointment_date(doctor_appointments, appointment_params["date"]) and
+        appointment_params["time"] != "" and
+        check_appointment_time(doctor_appointments, appointment_params["time"]) do
+        changeset =
+          socket.assigns.appointment
+          |> Appointments.change_appointment(appointment_params)
+          |> Map.put(:action, :validate)
+      {:noreply, assign(socket, changeset: changeset, already_taken: "Time slot already has been taken. Please find another one")}
+    else
+      changeset =
+        socket.assigns.appointment
+        |> Appointments.change_appointment(appointment_params)
+        |> Map.put(:action, :validate)
+
+      {:noreply, assign(socket, changeset: changeset, already_taken: false)}
+    end
   end
 
   def handle_event("save", %{"appointment" => appointment_params}, socket) do
-    save_appointment(socket, socket.assigns.action, appointment_params)
+    if socket.assigns.already_taken do
+      {:noreply, assign(socket, changeset: socket.assigns.changeset, already_taken: socket.assigns.already_taken)}
+    else
+      save_appointment(socket, socket.assigns.action, appointment_params)
+    end
   end
 
   defp save_appointment(socket, :edit, appointment_params) do
@@ -69,5 +93,23 @@ defmodule PhysioWeb.UserLive.AppointmentLive.FormComponent do
       Accounts.list_doctor()
       |> Enum.map(fn doctor -> %{id: doctor.id, name: doctor.doctor_profile.first_name} end)
     end
+  end
+
+  def check_appointment_date(appointments, input_date) do
+    {:ok, input_date} = Date.from_iso8601(input_date)
+
+    Enum.any?(appointments, fn appointment ->
+      appointment.date == input_date
+    end)
+    |> IO.inspect(label: "Working1")
+  end
+
+  def check_appointment_time(appointments, input_time) do
+    {:ok, input_time} = Time.from_iso8601("#{input_time}:00")
+
+    Enum.any?(appointments, fn appointment ->
+      appointment.time == input_time
+    end)
+    |> IO.inspect(label: "Working2")
   end
 end
